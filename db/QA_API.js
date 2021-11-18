4e1fc4abbcf54aee2fe253b409e96772d5424e7a
Jonathan Jewett <jonathanjewett0@gmail.com>
2021-11-18 12:52:05 -0600

    api file created and server index updated to import it

---
+++ db/QA_API.js
@@ -0,0 +1,8 @@
+const Product = require('./models/Product.js');
+const Question = require('./models/Question.js');
+const Answer = require('./models/Answer.js');
+const Answer_Photo = require('./models/Answer_Photo.js');
+
+const markHelpful = (question_id) => {
+  // do stuff
+};
--- server/index.js
+++ server/index.js
@@ -4,11 +4,23 @@ const Product = require('../db/models/Product.js');
 const Question = require('../db/models/Question.js');
 const Answer = require('../db/models/Answer.js');
 const Answer_Photo = require('../db/models/Answer_Photo.js');
+const QA_API = require('../models/QA_API.js');
 const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
   res.send('Hellow World!')
+});
+
+// PUT request to mark a question as helpful
+// Should increment necessary field by 1
+// Parameter - question_id
+// Type - Integer
+// request.params if after semicolon
+// if after question mark its req.query
+app.put('/qa/questions/:question_id/helpful', (req, res) => {
+  // call methods from API
+
 });
 
 app.listen(port, () => {
