31a7d1037d1f71b5c747ab270697e3accc676ea1
jonathanjewett <jonathanjewett0@gmail.com>
2021-11-19 18:13:58 -0600

    attempting to attach answers to questions

--- db/QA_API.js
+++ db/QA_API.js
@@ -4,6 +4,8 @@ const Answer = require('./models/Answer.js');
 const Answer_Photo = require('./models/Answer_Photo.js');
 
 const listQuestions = (product_id) => {
+  let questionsArray = [];
+  let answerArray = [];
   return Question.findAll({
     attributes: [
       'question_id',
@@ -14,8 +16,38 @@ const listQuestions = (product_id) => {
       'reported'
     ],
     where: {product_id: product_id}
-  }).then((results) => {
-    console.log(results);
+  }).then((questions) => {
+    //console.log(results);
+    // results is an array of Question objects
+    // Question {
+    //   dataValues: {
+    //     question_id: 2,
+    //     question_body: 'HEY THIS IS A WEIRD QUESTION!!!!?',
+    //     question_date: '1613888219613',
+    //     asker_name: 'jbilas',
+    //     question_helpfulness: 4,
+    //     reported: true
+    //   }
+    var answerQueries = [];
+    for (var i = 0; i < questions.length; i++) {
+      answerQueries.push(Answer.findAll({
+        attributes: [
+          'answer_id',
+          'answer_body',
+          'answer_date',
+          'answerer_name',
+          'answer_helpfulness'
+        ],
+        where: {question_id: questions[i].dataValues.question_id}
+      }));
+    }
+    // what if a question doesnt have any answers?
+    Promise.all(answerQueries).then((answers) => {
+      for (var p = 0; p < questions.length; p++) {
+        questions[p]['answers'] = answers[p];
+      }
+    });
+    return questions;
   });
 };
 
